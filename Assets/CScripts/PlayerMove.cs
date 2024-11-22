1e2e9593a8025ad385eed4b27e0320e1ec6f5113
hiro-k-8001 <hiro.k.8001.m@gmail.com>
2024-11-22 17:23:21 +0900

    walksound

--- Assets/CScripts/PlayerMove.cs
+++ Assets/CScripts/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -12,6 +13,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float runSpeed = 20f;
     [SerializeField] private StaminaBar staminaBar;
     [SerializeField] private bool isRunning;
+    private AudioSource audiowalk;
+    private AudioSource audiorun;
     private CharacterController charController;
 
     private void Awake()
@@ -23,6 +26,14 @@ public class PlayerMove : MonoBehaviour
     private void Start()
     {
         staminaBar = FindObjectOfType<StaminaBar>();
+        GameObject player = GameObject.Find("Player");
+
+        Transform walk = player.transform.Find("walk sound");
+        Transform run = player.transform.Find("run sound");
+
+        audiowalk = walk.GetComponent<AudioSource>();
+        audiorun = run.GetComponent<AudioSource>();
+      
     }
 
     private void Update()
@@ -31,7 +42,7 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
-            Debug.Log(this.transform.position);
+            //Debug.Log(this.transform.position);
         }
     }
 
@@ -45,30 +56,60 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space)) //スペースキー押しているとき
         {
-            if (!isRunning && staminaBar.CanStartRunning()) //スタミナ50以上のとき
+            if (!isRunning && staminaBar.CanStartRunning() && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)) //走っていなくて、スタミナ50以上、wasd入力がある
             {
                 isRunning = true;
                 staminaBar.SetRunning(true);
-                charController.SimpleMove((forwardMovement + rightMovement) * (runSpeed / movementSpeed)); // ダッシュ時の速度を調整
+                //charController.SimpleMove((forwardMovement + rightMovement) * (runSpeed / movementSpeed)); // ダッシュ時の速度を調整
+                audiorun.Play();
             }
         }
-        else
+        else //走っているときに、スペースキーを離したら走るのをやめるように。
         {
             isRunning = false;
             staminaBar.SetRunning(false);
-
-        }
-        if (isRunning && staminaBar.CanContinueRunning())
-        {
             
+        }
+
+
+        if (isRunning && staminaBar.CanContinueRunning())　//走っていて走り続けられるとき(スタミナ０になったら走れないように。)
+        {
+           
             charController.SimpleMove((forwardMovement + rightMovement) * (runSpeed / movementSpeed)); // ダッシュ時の速度を調整
+            // 走る音を再生（再生中でない場合のみ）
+            if (!audiorun.isPlaying)
+            {
+                audiorun.Play();
+            }
+
+            // 歩く音を止める
+            if (audiowalk.isPlaying)
+            {
+                audiowalk.Stop();
+            }
         }
         else
         {
             charController.SimpleMove(forwardMovement + rightMovement);
             isRunning = false;
             staminaBar.SetRunning(false);
+            // 歩く音を再生（再生中でない場合のみ）
+            if (!audiowalk.isPlaying && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+            {
+                audiowalk.Play();
+            }
+
+            // 走る音を止める
+            if (audiorun.isPlaying)
+            {
+                audiorun.Stop();
+            }
         }
-    
+
+        if (audiowalk.isPlaying && !(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+        {
+            audiowalk.Stop();
+        }
+
     }
 }
