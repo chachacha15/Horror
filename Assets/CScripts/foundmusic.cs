d444affaaf458b2583e8e08405bc0f3845ef75ba
hiro-k-8001 <hiro.k.8001.m@gmail.com>
2024-12-07 00:31:47 +0900

    foundsound

--- Assets/CScripts/foundmusic.cs
+++ Assets/CScripts/foundmusic.cs
@@ -1,74 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class foundmusic : MonoBehaviour
 {
     private GhostAI ghostAI; // GhostAIスクリプトへの参照
-    private AudioSource audioSource; // 音楽再生用のAudioSource
-    private bool isPlaying = false; // 音楽再生中かどうかを管理
+    private AudioSource bgmAudioSource; // BGM再生用のAudioSource
+    private AudioSource sfxAudioSource; // 効果音再生用のAudioSource
+    private bool isPlayingBGM = false; // BGM再生中かどうかを管理
+    private bool hasPlayedSFX = false; // 効果音を再生したかどうかを管理
 
     // Start is called before the first frame update
     void Start()
     {
-        // Ghost1の親オブジェクトからGhostAIスクリプトを取得
+        // 親オブジェクト Ghost1 から GhostAI スクリプトを取得
         Transform ghost1Transform = transform.parent; // "Ghost1" の Transform
         if (ghost1Transform != null)
         {
             ghostAI = ghost1Transform.GetComponent<GhostAI>();
-            if (ghostAI == null)
-            {
-               
-            }
-        }
-        else
-        {
-          
         }
 
-        // 子オブジェクト "Time_limit" を探してAudioSourceを取得
+        // 子オブジェクト "Time_limit" を探して BGM 用 AudioSource を取得
         Transform timeLimitObject = transform.Find("Time_limit");
         if (timeLimitObject != null)
         {
-            audioSource = timeLimitObject.GetComponent<AudioSource>();
-            if (audioSource == null)
-            {
-              
-            }
-        }
-        else
-        {
-           
+            bgmAudioSource = timeLimitObject.GetComponent<AudioSource>();
         }
 
-        // 音楽のループ設定
-        if (audioSource != null)
+        // 子オブジェクト "found" を探して効果音用 AudioSource を取得
+        Transform foundObject = transform.Find("found");
+        if (foundObject != null)
         {
-            audioSource.loop = true; // 音楽をループ再生
+            sfxAudioSource = foundObject.GetComponent<AudioSource>();
+        }
+
+        // BGMのループ設定
+        if (bgmAudioSource != null)
+        {
+            bgmAudioSource.loop = true; // BGMをループ再生
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ghostAI != null && audioSource != null)
+        if (ghostAI != null && bgmAudioSource != null && sfxAudioSource != null)
         {
-            if (ghostAI.sensor) // sensorがtrueなら音楽を再生
+            if (ghostAI.sensor) // sensorがtrueならBGMを再生
             {
-                if (!isPlaying)
+                if (!isPlayingBGM)
                 {
-                    audioSource.Play();
-                    isPlaying = true;
+                    bgmAudioSource.Play();
+                    isPlayingBGM = true;
+                }
+
+                if (!hasPlayedSFX) // 効果音がまだ再生されていない場合
+                {
+                    sfxAudioSource.PlayOneShot(sfxAudioSource.clip); // 効果音を一度だけ再生
+                    hasPlayedSFX = true;
                 }
             }
-            else // sensorがfalseなら音楽を停止
+            else // sensorがfalseならBGMを停止
             {
-                if (isPlaying)
+                if (isPlayingBGM)
                 {
-                    audioSource.Stop();
-                    isPlaying = false;
+                    bgmAudioSource.Stop();
+                    isPlayingBGM = false;
                 }
+
+                hasPlayedSFX = false; // 効果音の再生状態をリセット
             }
         }
     }
